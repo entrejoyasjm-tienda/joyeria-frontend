53a18bcf9d7468fd1fc12a76200ac136884d5ad8
Lucas-Villarrea <villarreallucas@gmail.com>
2026-08-03 00:13:06 -0300

    fix: actualizar manualChunks como funcion en vite.config.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'https://joyeria-backend-smkb.onrender.com/api',
+headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Este interceptor revisa si hay un token guardado en el navegador y lo envía en los headers
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuración estándar para Vite 5
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+    rollupOptions: {
+      output: {
+        // 🛠️ 'manualChunks' configurado como FUNCIÓN en lugar de Objeto
+        manualChunks(id) {
+          // Separa las librerías de node_modules en un chunk independiente
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
