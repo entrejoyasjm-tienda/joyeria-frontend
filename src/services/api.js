22f5fc17c8c01cd7b35d5cfd34f55db3748a87ad
Lucas-Villarrea <villarreallucas@gmail.com>
2026-08-03 12:31:48 -0300

    fix: remover /api de baseURL en api.js para evitar error 404

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://joyeria-backend-smkb.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://joyeria-backend-smkb.onrender.com',
 headers: {
     'Content-Type': 'application/json',
   },
